14cfd53b4ed99b79cb177ac4e48871b9beb62957
Shane Stroud <shanemstroud@gmail.com>
2022-10-28 16:55:30 -0400

    Update Config
    password

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,2 @@
-export const MongoURI = "mongodb+srv://shanemstroud:<Shane1234>@midtermcomp229.fvmsw8f.mongodb.net/media?retryWrites=true&w=majority";
+export const MongoURI = "mongodb+srv://shanemstroud:Shane1234@midtermcomp229.fvmsw8f.mongodb.net/media?retryWrites=true&w=majority";
 export const Secret = "someSecret";
