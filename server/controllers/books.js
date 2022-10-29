a8afa805fc3cf5d7383ab68f51b0d3b2ff605cdb
Shane Stroud <shanemstroud@gmail.com>
2022-10-28 20:10:09 -0400

    added edit and delete buttons + functionality
    edit sends user to edits page allows change of information, updates database. delete button removes selected item from database

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -43,22 +43,56 @@ export function processAddPage(req, res, next) {
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id;
+
+    booksModel.findById(id, (err, book) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Edit Book', page: 'books/edit', book: book });
+    });    
 
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    
+  
+    let id = req.params.id;
+    
+    let newBook = ({
+        _id: req.body.id,
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+        
+    });
+
+    booksModel.updateOne({_id: id }, newBook, (err, Book) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/books/list')
+    } )
+    
 }
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    let id = req.params.id;
+
+    booksModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/books/list');
+    })
 }
